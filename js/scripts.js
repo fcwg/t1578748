acd8df7904c918027057c61c0eeb6f66d5adf5e5
fcwg <fcwong@yahoo.com>
2016-01-19 22:35:14 -0800

    added: fitness pic, elites etc

---
+++ js/scripts.js
@@ -0,0 +1,287 @@
+﻿jQuery(function($){
+
+'use strict';
+
+
+// news ticker Begin
+
+
+
+
+
+    // ----------------------------------------------
+    // Preloader
+    // ----------------------------------------------
+	(function () {
+	    $(window).load(function() {
+	        $('#pre-status').fadeOut();
+	        $('#st-preloader').delay(350).fadeOut('slow');
+	    });
+	}());
+
+
+
+    // ---------------------------------------------- 
+    //  magnific-popup
+    // ----------------------------------------------
+	(function () {
+
+		$('.portfolio-items').magnificPopup({ 
+			delegate: 'a',
+			type: 'image',
+			// other options
+			closeOnContentClick: false,
+			closeBtnInside: false,
+			mainClass: 'mfp-with-zoom mfp-img-mobile',
+
+			gallery: {
+				enabled: false
+			},
+			zoom: {
+				enabled: true,
+				duration: 300, // don't foget to change the duration also in CSS
+				opener: function(element) {
+					return element.find('i');
+				}
+			}
+
+		});
+
+	}()); 
+
+		// button hover
+
+		$(".button-fill").hover(function() {
+		   $(this).children(".button-inside").addClass('full');
+		 }, function() {
+		   $(this).children(".button-inside").removeClass('full');
+		 });
+
+
+// dropdown menu
+var dropdown = document.querySelectorAll('.dropdown');
+var dropdownArray = Array.prototype.slice.call(dropdown,0);
+dropdownArray.forEach(function(el){
+	var button = el.querySelector('a[data-toggle="dropdown"]'),
+			menu = el.querySelector('.dropdown-menu'),
+			arrow = button.querySelector('i.icon-arrow');
+
+	button.onclick = function(event) {
+		if(!menu.hasClass('show')) {
+			menu.classList.add('show');
+			menu.classList.remove('hide');
+			arrow.classList.add('open');
+			arrow.classList.remove('close');
+			event.preventDefault();
+		}
+		else {
+			menu.classList.remove('show');
+			menu.classList.add('hide');
+			arrow.classList.remove('open');
+			arrow.classList.add('close');
+			event.preventDefault();
+		}
+	};
+})
+
+Element.prototype.hasClass = function(className) {
+    return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
+};
+
+// dropdown menu
+
+    // ---------------------------------------------- 
+    // Fun facts
+    // ---------------------------------------------- 
+	(function () {
+		$('.st-counter').counterUp({
+		    delay: 10,
+		    time: 1500
+		});
+	}()); 
+
+
+
+    // ---------------------------------------------- 
+    //  Isotope Filter 
+    // ---------------------------------------------- 
+	(function () {
+		var winDow = $(window);
+		var $container=$('.portfolio-items');
+		var $filter=$('.filter');
+
+		try{
+			$container.imagesLoaded( function(){
+				$container.show();
+				$container.isotope({
+					filter:'*',
+					layoutMode:'masonry',
+					animationOptions:{
+						duration:750,
+						easing:'linear'
+					}
+				});
+			});
+		} catch(err) {
+		}
+
+		winDow.bind('resize', function(){
+			var selector = $filter.find('a.active').attr('data-filter');
+
+			try {
+				$container.isotope({ 
+					filter	: selector,
+					animationOptions: {
+						duration: 750,
+						easing	: 'linear',
+						queue	: false,
+					}
+				});
+			} catch(err) {
+			}
+			return false;
+		});
+
+		$filter.find('a').click(function(){
+			var selector = $(this).attr('data-filter');
+
+			try {
+				$container.isotope({ 
+					filter	: selector,
+					animationOptions: {
+						duration: 750,
+						easing	: 'linear',
+						queue	: false,
+					}
+				});
+			} catch(err) {
+
+			}
+			return false;
+		});
+
+
+		var filterItemA	= $('.filter a');
+
+		filterItemA.on('click', function(){
+			var $this = $(this);
+			if ( !$this.hasClass('active')) {
+				filterItemA.removeClass('active');
+				$this.addClass('active');
+			}
+		});
+	}()); 
+
+
+// Begin news ticker
+
+$('.newstape').newstape({
+
+// timer period
+period: 30, 
+
+// offset pixel count
+offset: 1, 
+
+// mousewheel scrolling
+mousewheel: true, 
+
+// mousewheel offset pixel count
+mousewheelRate: 30, 
+
+// dragging tape content
+dragable: true
+
+});
+
+
+// End news ticker
+
+
+	// -------------------------------------------------------------
+    // masonry
+    // -------------------------------------------------------------
+
+    (function () {
+		var $container = $('.portfolio-items');
+		// initialize
+		$container.masonry({
+		  itemSelector: '.work-grid'
+		});
+    }());
+
+
+  	// -------------------------------------------------------------
+    // Animated scrolling / Scroll Up
+    // -------------------------------------------------------------
+
+    (function () {
+        $('li a[href*=#]').bind("click", function(e){
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top -79
+            }, 1000);
+            e.preventDefault();
+        });
+    }());
+
+
+    // ----------------------------------------------
+    // Owl Carousel
+    // ----------------------------------------------
+	(function () {
+
+		$(".st-testimonials").owlCarousel({
+		singleItem:true,
+		lazyLoad : true,
+		pagination:false,
+		navigation : false,
+		autoPlay: true,
+		});
+
+	}());
+
+
+    // -------------------------------------------------------------
+    // Back To Top
+    // -------------------------------------------------------------
+
+    (function () {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('.scroll-up').fadeIn();
+            } else {
+                $('.scroll-up').fadeOut();
+            }
+        });
+    }());
+	
+
+    // ----------------------------------------------
+    // Parallax Scrolling
+    // ----------------------------------------------
+	(function () {
+		$(window).bind('load', function () {
+			parallaxInit();						  
+		});
+		function parallaxInit() {		
+			$("#testimonial").parallax("50%", 0.3);
+		}	
+		parallaxInit();
+	}());
+
+	
+
+    // ----------------------------------------------
+    // fitvids js
+    // ----------------------------------------------
+    (function () {
+
+        $(".post-video").fitVids();
+
+    }()); 
+
+
+	
+
+});
