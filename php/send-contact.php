0d15cdbd63b25b997540dd42bc9e9e4467e85a2c
fcwg <fcwong@yahoo.com>
2016-01-25 01:42:26 -0800

    launch 1

--- php/send-contact.php
+++ php/send-contact.php
@@ -7,7 +7,7 @@
 	$message = @trim(stripslashes($_POST['message'])); 
 
 	$email_from = $email;
-	$email_to = 'email@yourdomain.com';//replace with your email
+	$email_to = 'rtompkinstennis@aol.com';//replace with your email
 
 	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Website: ' . $website . "\n\n" . 'Message: ' . $message;
 
@@ -18,6 +18,6 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
-	<script>alert("Thank you for contact us. As early as possible  we will contact you.");</script>
+	<script>alert("Thank you for contacting us! We greatly value your business, and we'll be in touch shortly!");</script>
 	<meta HTTP-EQUIV="REFRESH" content="0; url=../index.html"> 
 </head>
