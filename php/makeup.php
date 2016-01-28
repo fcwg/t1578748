6afd4d2ecb86c598b709c41a6129c104dd3efb26
fcwg <fcwong@yahoo.com>
2016-01-28 01:21:19 -0800

    added fitness.html and more

--- php/makeup.php
+++ php/makeup.php
@@ -7,7 +7,7 @@
 	$message = @trim(stripslashes($_POST['message'])); 
 
 	$email_from = $email;
-	$email_to = 'fcwong@yahoo.com';//replace with your email
+	$email_to = 'cherie@tompkinstennis.com';//replace with your email
 
 	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Website: ' . $website . "\n\n" . 'Message: ' . $message;
 
@@ -18,6 +18,6 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
-	<script>alert("Thank you for contact us. As early as possible  we will contact you.");</script>
+	<script>alert("Thank you for contacting us. We value your business and will be in touch shortly!");</script>
 	<meta HTTP-EQUIV="REFRESH" content="0; url=../makeup.html"> 
 </head>
